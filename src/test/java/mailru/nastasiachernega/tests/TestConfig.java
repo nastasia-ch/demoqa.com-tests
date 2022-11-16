66181c84767fd0125073d950a202eada109a1659
nastasia-ch <nastasia-chernega@mail.ru>
2022-11-16 03:15:40 +0300

    add code of attaching video in Allure Report

--- src/test/java/mailru/nastasiachernega/tests/TestConfig.java
+++ src/test/java/mailru/nastasiachernega/tests/TestConfig.java
@@ -7,13 +7,22 @@ import mailru.nastasiachernega.helpers.Attach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestConfig {
 
     @BeforeAll
     static void setUp() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//      capabilities.setCapability("browserName", "chrome");
+//      capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.holdBrowserOpen = true;
+//      Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
@@ -23,6 +32,7 @@ public class TestConfig {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
