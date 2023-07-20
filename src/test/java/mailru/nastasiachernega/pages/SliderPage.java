5745e7d2d86ba509bace125c8dc0848134882aee
nastasia-ch <nastasia-chernega@mail.ru>
2023-07-21 00:35:54 +0300

    add TestsSlider()

---
+++ src/test/java/mailru/nastasiachernega/pages/SliderPage.java
@@ -0,0 +1,54 @@
+package mailru.nastasiachernega.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class SliderPage {
+
+    private SelenideElement
+            slider = $(".range-slider"),
+            valueInput = $("#sliderValue");
+
+    private final static String TITLE_TEXT = "Slider";
+
+    public SliderPage openPage() {
+        open("/slider");
+        $(".main-header").shouldHave(text(TITLE_TEXT));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        return this;
+    }
+
+    public SliderPage moveSliderRight(int newValue) {
+        int currentValue = Integer.parseInt(slider.getAttribute("value"));
+        int differenceOfValues = newValue - currentValue;
+        for (int i = 0; i < differenceOfValues; i++) {
+            slider.sendKeys(Keys.ARROW_RIGHT);
+        }
+        return this;
+    }
+
+    public SliderPage moveSliderLeft(int newValue) {
+        int currentValue = Integer.parseInt(slider.getAttribute("value"));
+        int differenceOfValues = currentValue - newValue;
+        for (int i = 0; i < differenceOfValues; i++) {
+            slider.sendKeys(Keys.ARROW_LEFT);
+        }
+        return this;
+    }
+
+    public SliderPage checkSliderPosition(int value) {
+        slider.shouldHave(attribute("value", value+""));
+        valueInput.shouldHave(attribute("value", value+""));
+        return this;
+    }
+
+
+
+
+}
---
+++ src/test/java/mailru/nastasiachernega/tests/TestsSlider.java
@@ -0,0 +1,80 @@
+package mailru.nastasiachernega.tests;
+
+import io.qameta.allure.*;
+import mailru.nastasiachernega.config.WebDriverProvider;
+import mailru.nastasiachernega.pages.SliderPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static io.qameta.allure.Allure.step;
+
+public class TestsSlider extends WebDriverProvider {
+
+    SliderPage sliderPage = new SliderPage();
+
+    @Epic("demoqa.com")
+    @Feature("Slider")
+    @Story("Перемещение слайдера ВПРАВО")
+    @Owner("Anastasia Chernega")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Ссылка на Slider", url = "https://demoqa.com/slider")
+    @DisplayName("Перемещение слайдера ВПРАВО")
+    @CsvSource(value = {
+            "68",
+            "100"
+    })
+    @ParameterizedTest
+    void moveSliderRight(int newValue) {
+
+        step("Открываем раздел Slider", () -> {
+            sliderPage.openPage();
+        });
+
+        step("Проверяем начальную позицию слайдера: должна быть 25", () -> {
+            sliderPage.checkSliderPosition(25);
+        });
+
+        step("Перемещаем слайдер в новую позицию: " + newValue, () -> {
+            sliderPage.moveSliderRight(newValue);
+        });
+
+        step("Проверяем новую позицию слайдера: должна быть " + newValue, () -> {
+            sliderPage.checkSliderPosition(newValue);
+        });
+
+    }
+
+    @Epic("demoqa.com")
+    @Feature("Slider")
+    @Story("Перемещение слайдера ВЛЕВО")
+    @Owner("Anastasia Chernega")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Ссылка на Slider", url = "https://demoqa.com/slider")
+    @DisplayName("Перемещение слайдера ВЛЕВО")
+    @CsvSource(value = {
+            "13",
+            "0"
+    })
+    @ParameterizedTest
+    void moveSliderLeft(int newValue) {
+
+        step("Открываем раздел Slider", () -> {
+            sliderPage.openPage();
+        });
+
+        step("Проверяем начальную позицию слайдера: должна быть 25", () -> {
+            sliderPage.checkSliderPosition(25);
+        });
+
+        step("Перемещаем слайдер в новую позицию: " + newValue, () -> {
+            sliderPage.moveSliderLeft(newValue);
+        });
+
+        step("Проверяем новую позицию слайдера: должна быть " + newValue, () -> {
+            sliderPage.checkSliderPosition(newValue);
+        });
+
+    }
+
+}
